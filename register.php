0ca3dd4501639b81b5998ebb97e0407bad63a724
Yasz Avellia <zakamaragames@gmail.com>
2022-08-23 17:30:42 +0700

    hias register

--- register.php
+++ register.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 $conn = mysqli_connect('localhost', 'root', '', 'db_logintest');
 
-if(isset($_POST['btn-register'])){
+if (isset($_POST['btn-register'])) {
     $nama = htmlspecialchars($_POST['nama']);
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['password']);
-    
-    if($nama == "" or $username == "" or $password == ""){
+
+    if ($nama == "" or $username == "" or $password == "") {
         $alert = "Masukan Data Dengan Lengkap";
     } else {
         mysqli_query($conn, "insert into tbl_admin (`nama`, `username`, `password`) VALUES ('$nama', '$username', '$password') ");
@@ -18,26 +18,39 @@ if(isset($_POST['btn-register'])){
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
+
+<body>
     <style>
-        .form-regis span{
+        .form-regis span {
             height: 16px;
         }
 
+        .reg {
+            background: #007AFF;
+            border: none;
+            outline: none;
+            box-shadow: none;
+            color: whitesmoke;
+            padding: 6px 85px;
+            border-radius: 25px;
+            font-family: sans-serif;
+            letter-spacing: 0.5px;
+        }
     </style>
 
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-5">
-                <div class="card shadow-lg">
+                <div class="card shadow-lg pb-4">
                     <div class="card-body">
-                        <div class="header">
+                        <div class="header text-center">
                             <h1>Register</h1>
                         </div>
                         <form action="" method="post">
@@ -49,12 +62,12 @@ if(isset($_POST['btn-register'])){
                                 <label for="">Password</label>
                                 <input type="password" name="password" placeholder="Masukan Password Anda" class="form-control">
                                 <span>
-                                    <p><?php if(isset($alert)){
-                                        echo $alert;
-                                    } ?></p>
+                                    <p><?php if (isset($alert)) {
+                                            echo $alert;
+                                        } ?></p>
                                 </span>
-                                <div class="but">
-                                    <button class="btn btn-primary" type="submit" name="btn-register">Register</button>
+                                <div class="but text-center">
+                                    <button class="btn btn-primary reg" type="submit" name="btn-register">Register</button>
                                 </div>
                             </div>
                         </form>
@@ -64,5 +77,6 @@ if(isset($_POST['btn-register'])){
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
